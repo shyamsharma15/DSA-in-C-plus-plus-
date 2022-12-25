7d6dffb68c1882ded7024611b31f9b7b1dc1b1d4
Shyam Sharma <121392308+shyamsharma15@users.noreply.github.com>
2022-12-25 23:01:46 +0530

    Program for Selection Sort

---
+++ selectionsort.cpp
@@ -0,0 +1,39 @@
+// Program for sellection sort :) ....
+
+/*
+#include<iostream>
+using namespace std;
+void selectionSort(int arr[] , int size )
+{
+    for(int i= 0;i<size;i++)
+    {
+        int minIndex = i;
+        for(int j = i+1;j<size;j++)
+        {
+           if(arr[j] < arr[minIndex])
+           minIndex = j;
+           
+        }
+        swap(arr[minIndex] , arr[i]);
+    }
+
+}
+void printArray(int arr[] , int size )
+{
+    for(int i = 0;i<size ; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main()
+{
+    int arr[6] = { 2,5,5,6,4,9,};
+    cout<<" the array after sorting is "<<endl;
+    selectionSort(arr , 6);
+    printArray(arr , 6);
+}
+
+
+*/  
+
